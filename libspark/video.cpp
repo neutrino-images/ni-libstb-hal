56874d9721cce5422b8e1cdb9e0d727311d8094a
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2012-04-05 11:58:05 +0200

    spark: temporary (for testing) map "auto" video mode to 1080p50

--- libspark/video.cpp
+++ libspark/video.cpp
@@ -303,7 +303,7 @@ int cVideo::SetVideoSystem(int video_system, bool remember)
 		"1080p30",	// VIDEO_STD_1080P30
 		"1080p24",	// VIDEO_STD_1080P24
 		"1080p25",	// VIDEO_STD_1080P25
-		"1080i50"	// VIDEO_STD_AUTO...
+		"1080p50"	// VIDEO_STD_1080P50
 	};
 
 	if (video_system > VIDEO_STD_MAX)

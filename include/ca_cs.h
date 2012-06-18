c50c2888ce23b4220ff3e8808ab80166fa08cb11
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2012-06-18 11:13:33 +0200

    move dummy cCA implementation to commmon/ directory

--- include/ca_cs.h
+++ include/ca_cs.h
@@ -1,8 +1,1 @@
-#include <config.h>
-#if HAVE_TRIPLEDRAGON
-#include "../libtriple/ca_cs.h"
-#elif HAVE_SPARK_HARDWARE
-#include "../libspark/ca_cs.h"
-#else
-#error neither HAVE_TRIPLEDRAGON nor HAVE_SPARK_HARDWARE defined
-#endif
+#include "../common/ca.h"

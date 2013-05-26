236c2401f1dd850bcb5c034df05865f3285d97ba
martii <m4rtii@gmx.de>
2013-05-26 14:24:31 +0200

    revert audio_mixer changes

--- libspark/audio_mixer.h
+++ libspark/audio_mixer.h
@@ -28,21 +28,6 @@ class mixerVolume
 		snd_mixer_t *handle;
 		snd_mixer_elem_t* elem;
 		snd_mixer_selem_id_t *sid;
-
-		void *alsaLib;
-		int (*snd_card_get_index)(const char *name);
-		int (*snd_mixer_open)(snd_mixer_t **mixer, int mode);
-		int (*snd_mixer_attach)(snd_mixer_t *mixer, const char *name);
-		int (*snd_mixer_selem_register)(snd_mixer_t *mixer, struct snd_mixer_selem_regopt *options, snd_mixer_class_t **classp);
-		int (*snd_mixer_load)(snd_mixer_t *mixer);
-		void (*snd_mixer_selem_id_set_index)(snd_mixer_selem_id_t *obj, unsigned int val);
-		void (*snd_mixer_selem_id_set_name)(snd_mixer_selem_id_t *obj, const char *val);
-		snd_mixer_elem_t *(*snd_mixer_find_selem)(snd_mixer_t *mixer, const snd_mixer_selem_id_t *id);
-		int (*snd_mixer_selem_get_playback_volume_range)(snd_mixer_elem_t *elem, long *min, long *max);
-		int (*snd_mixer_close)(snd_mixer_t *mixer);
-		void (*snd_mixer_selem_id_free)(snd_mixer_selem_id_t *obj);
-		int (*snd_mixer_selem_set_playback_volume_all)(snd_mixer_elem_t *elem, long value);
-		size_t (*snd_mixer_selem_id_sizeof)(void);
 	public:
 		mixerVolume(const char *selem_name, const char *Card, long volume = -1);
 		~mixerVolume(void);

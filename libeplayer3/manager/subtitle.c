f199c329ad93bc10d50a7e9e43c514b2af50f6ab
martii <m4rtii@gmx.de>
2013-07-26 20:29:01 +0200

    libeplayer3/subtitle: reduce debugging level

--- libeplayer3/manager/subtitle.c
+++ libeplayer3/manager/subtitle.c
@@ -36,7 +36,7 @@
 
 #ifdef SUBTITLE_MGR_DEBUG
 
-static short debug_level = 20;
+static short debug_level = 10;
 
 #define subtitle_mgr_printf(level, x...) do { \
 if (debug_level >= level) printf(x); } while (0)

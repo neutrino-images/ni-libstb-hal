ecf5bb30fb008da9f586dba08d8fee1e4de31555
martii <m4rtii@gmx.de>
2013-11-19 19:33:33 +0100

    libeplayer3/ffmpeg: add 3gp and ogm extensions

--- libeplayer3/container/container_ffmpeg.c
+++ libeplayer3/container/container_ffmpeg.c
@@ -1743,7 +1743,7 @@ static int Command(void  *_context, ContainerCmd_t command, void * argument)
     return ret;
 }
 
-static char *FFMPEG_Capabilities[] = {"avi", "mkv", "mp4", "ts", "mov", "flv", "flac", "mp3", "mpg", "m2ts", "vob", "wmv","wma", "asf", "mp2", "m4v", "m4a", "divx", "dat", "mpeg", "trp", "mts", "vdr", "ogg", "wav", "wtv", NULL };
+static char *FFMPEG_Capabilities[] = {"avi", "mkv", "mp4", "ts", "mov", "flv", "flac", "mp3", "mpg", "m2ts", "vob", "wmv","wma", "asf", "mp2", "m4v", "m4a", "divx", "dat", "mpeg", "trp", "mts", "vdr", "ogg", "wav", "wtv", "ogm", "3gp", NULL };
 
 Container_t FFMPEGContainer = {
     "FFMPEG",

c20a8fa5aff279551496b01511f77badc8fad748
max10 <max_10@gmx.de>
2014-11-24 11:07:22 +0100

    Revert "disable logging"
    This reverts commit 4798ce2517c516868858c1a8db1469be05f19923.

--- libeplayer3/input.cpp
+++ libeplayer3/input.cpp
@@ -20,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#define ENABLE_LOGGING 0
+#define ENABLE_LOGGING 1
 
 #include <stdio.h>
 #include <stdlib.h>

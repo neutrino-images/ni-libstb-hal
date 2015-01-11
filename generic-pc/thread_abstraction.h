78dd12e2dd8b9661c4e749bb547e606aa530cf01
smogm <smogm@vh0st.me>
2015-01-11 17:52:31 +0100

    add mutex and thread abstraction

---
+++ generic-pc/thread_abstraction.h
@@ -0,0 +1,25 @@
+#ifndef _THREAD_ABSTRACTION_H
+#define _THREAD_ABSTRACTION_H
+
+#include <pthread.h>
+
+class Thread
+{
+	bool mIsRunning;
+	pthread_t mThread;
+
+	static void* runThread(void*);
+	Thread(const Thread&);
+	const Thread& operator=(const Thread&);
+
+	public:
+		Thread();
+		~Thread();
+		void startThread();
+		void joinThread();
+
+	protected:
+		virtual void run() = 0;
+};
+
+#endif

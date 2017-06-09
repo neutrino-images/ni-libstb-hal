23afdef65c2442eeabb4760c274372d86fd065ed
TangoCash <eric@loxat.de>
2017-06-09 13:25:49 +0200

    implement GetVideoSystem

--- libspark/video_lib.h
+++ libspark/video_lib.h
@@ -182,6 +182,9 @@ class cVideo
 		int Stop(bool blank = true);
 		bool Pause(void);
 
+		/* get video system infos */
+		int GetVideoSystem();
+
 		/* set video_system */
 		int SetVideoSystem(int video_system, bool remember = true);
 		int SetStreamType(VIDEO_FORMAT type);

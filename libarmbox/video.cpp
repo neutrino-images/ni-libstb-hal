57ae0340c839ce142f68f0109602a9abaa6488cd
TangoCash <eric@loxat.de>
2017-10-01 11:56:28 +0200

    adding 4k video settings for armbox

--- libarmbox/video.cpp
+++ libarmbox/video.cpp
@@ -394,6 +394,10 @@ int cVideo::SetVideoSystem(int video_system, bool remember)
 		"1080p60",	// VIDEO_STD_1080P60
 		"1080p2397",	// VIDEO_STD_1080P2397
 		"1080p2997",	// VIDEO_STD_1080P2997
+		"2160p24",	//VIDEO_STD_2160P24
+		"2160p25",	// VIDEO_STD_2160P25
+		"2160p30",	// VIDEO_STD_2160P30
+		"2160p50",	// VIDEO_STD_2160P50
 		"720p50"	// VIDEO_STD_AUTO
 	};
 

36afd077fe676146350b85163a29fa8f11708b35
Jacek Jendrzej <satbaby@kawaii.com>
2017-11-16 19:10:40 +0100

    fix last commit

--- libarmbox/video.cpp
+++ libarmbox/video.cpp
@@ -238,7 +238,7 @@ int image_to_mpeg2(const char *image_name, const char *encode_name)
 	avcodec_register_all();
 
 	AVFormatContext *formatContext = avformat_alloc_context();
-	if (formatContext && (ret = avformat_open_input(&formatContext, path, NULL, NULL)) == 0){
+	if (formatContext && (ret = avformat_open_input(&formatContext, image_name, NULL, NULL)) == 0){
 		AVCodecContext *codecContext = open_codec(AVMEDIA_TYPE_VIDEO, formatContext);
 		if(codecContext){
 			AVPacket packet;

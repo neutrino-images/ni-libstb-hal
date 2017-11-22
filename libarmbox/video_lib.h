3ac8fa20809908fc3206d740787b4f4f1d70c0b8
Jacek Jendrzej <satbaby@kawaii.com>
2017-11-22 10:10:01 +0100

    write m2v direct to device

--- libarmbox/video_lib.h
+++ libarmbox/video_lib.h
@@ -232,7 +232,7 @@ class cVideo
 		void SendCECMessage(struct cec_message &message);
 		void SetCECState(bool state);
 		void ReportPhysicalAddress();
-		void ShowPicture(const char * fname, const char *_destname = NULL);
+		void ShowPicture(const char * fname);
 		void StopPicture();
 		void Standby(unsigned int bOn);
 		void Pig(int x, int y, int w, int h, int osd_w = 1064, int osd_h = 600, int startx = 0, int starty = 0, int endx = 1279, int endy = 719);

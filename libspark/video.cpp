eaf3e5c4b0bb96bd5bc8b42997b8616c3fcd98a8
GetAway <get-away@t-online.de>
2018-04-26 09:10:04 +0200

    fix possible segfault
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- libspark/video.cpp
+++ libspark/video.cpp
@@ -361,7 +361,9 @@ int cVideo::setAspectRatio(int aspect, int mode)
 	static const char *a[] = { "n/a", "4:3", "14:9", "16:9" };
 	static const char *m[] = { "panscan", "letterbox", "bestfit", "nonlinear", "(unset)" };
 	int n;
-	lt_debug("%s: a:%d m:%d  %s\n", __func__, aspect, mode, m[(mode < 0||mode > 3) ? 4 : mode]);
+
+	int mo = (mode < 0||mode > 3) ? 4 : mode;
+	lt_debug("%s: a:%d m:%d  %s\n", __func__, aspect, mode, m[mo]);
 
 	if (aspect > 3 || aspect == 0)
 		lt_info("%s: invalid aspect: %d\n", __func__, aspect);
@@ -376,8 +378,8 @@ int cVideo::setAspectRatio(int aspect, int mode)
 	if (mode == -1)
 		return 0;
 
-	lt_debug("%s: /proc/stb/video/policy -> %s\n", __func__, m[mode]);
-	n = proc_put("/proc/stb/video/policy", m[mode], strlen(m[mode]));
+	lt_debug("%s: /proc/stb/video/policy -> %s\n", __func__, m[mo]);
+	n = proc_put("/proc/stb/video/policy", m[mo], strlen(m[mo]));
 	if (n < 0)
 		return 1;
 	return 0;

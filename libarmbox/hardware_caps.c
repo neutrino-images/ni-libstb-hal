35f15585ed5fc227fd918b042276cb41a998ad3f
BPanther <bpanther_ts@hotmail.com>
2018-08-28 07:46:08 +0200

    vusolo4k: small fix - but not yet ready

--- libarmbox/hardware_caps.c
+++ libarmbox/hardware_caps.c
@@ -7,6 +7,7 @@
  * License: GPL v2 or later
  */
 
+#include <config.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -28,6 +29,23 @@ hw_caps_t *get_hwcaps(void)
 
 	memset(&caps, 0, sizeof(hw_caps_t));
 
+#if BOXMODEL_VUSOLO4K
+	initialized = 1;
+	caps.has_CI = 1;
+	caps.can_cec = 1;
+	caps.can_shutdown = 1;
+	caps.display_xres = 480;
+	caps.display_yres = 320;
+	caps.display_type = HW_DISPLAY_GFX;
+	caps.display_can_deepstandby = 1;
+	caps.display_can_set_brightness = 1;
+	caps.display_has_statusline = 1;
+	caps.has_button_timer = 1;
+	caps.has_HDMI = 1;
+	strcpy(caps.boxvendor, "VU");
+	strcpy(caps.boxname, "SOLO4K");
+	strcpy(caps.boxarch, "BCM7376");
+#else
 	initialized = 1;
 	caps.has_CI = 1;
 	caps.can_cec = 1;
@@ -42,5 +60,6 @@ hw_caps_t *get_hwcaps(void)
 	strcpy(caps.boxvendor, "AX-Technologies");
 	strcpy(caps.boxname, "HD51");
 	strcpy(caps.boxarch, "BCM7251S");
+#endif
 	return &caps;
 }

0de0b60cb1d43f978955a8368ce5579b532173d0
BPanther <BPanther@users.noreply.github.com>
2018-09-27 13:23:43 +0200

    fix/update hwcaps for hl101

--- libduckbox/hardware_caps.c
+++ libduckbox/hardware_caps.c
@@ -283,13 +283,13 @@ hw_caps_t *get_hwcaps(void)
 			caps.can_shutdown = 1;
 			caps.has_HDMI = 1;
 			caps.has_SCART = 1;
-			caps.has_SCART_input = 1,
-			caps.can_cec = 2;
+			caps.has_SCART_input = 1;
+			caps.can_cec = 1;
 			caps.has_fan = 0;
 			caps.has_CI = 2;
 			caps.display_can_set_brightness = 1;
 			caps.display_type = HW_DISPLAY_LINE_TEXT;
-			caps.display_xres = 8;			
+			caps.display_xres = 8;
 		}
 		else if (!strncmp(buf, "arivalink200", 12)) {
 			strcpy(caps.boxvendor, "DUCKBOX");

b6019d4f652a1323f808b33b65fc26b1ec7d888a
samsamsam <samsamsam@o2.pl>
2019-02-02 18:44:05 +0100

    Fix VP9 playback for the Octagon SF4008

--- libeplayer3-arm/output/writer/common/misc.c
+++ libeplayer3-arm/output/writer/common/misc.c
@@ -137,8 +137,12 @@ stb_type_t GetSTBType()
 		{
 			type = STB_DREAMBOX;
 		}
-		else if (access("/proc/stb/info/vumodel", F_OK) != -1)
+		else if (access("/proc/stb/info/vumodel", F_OK) != -1 && \
+		         access("/proc/stb/info/boxtype", F_OK) == -1)
 		{
+			// some STB like Octagon SF4008 has also /proc/stb/info/vumodel
+			// but VU PLUS does not have /proc/stb/info/boxtype
+			// please see: https://gitlab.com/e2i/e2iplayer/issues/282
 			type = STB_VUPLUS;
 		}
 		else if (access("/sys/firmware/devicetree/base/soc/hisilicon_clock/name", F_OK) != -1)

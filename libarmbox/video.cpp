a94547e7a37aaa815491a1a1517fb2af6cc6bf40
GetAway <get-away@t-online.de>
2019-03-12 21:59:42 +0100

    arm: fix black screen in channel swiching
    this fixes wrong commit e20d1913

--- libarmbox/video.cpp
+++ libarmbox/video.cpp
@@ -927,7 +927,7 @@ void cVideo::SetControl(int control, int value)
 	case VIDEO_CONTROL_ZAPPING_MODE:
 		zapping_mode = value;
 		const char *mode_zapping[] = { "hold", "mute" };
-		proc_put("/proc/stb/video/zapping_mode", mode_zapping[zapping_mode], strlen(mode_zapping[zapping_mode]));
+		proc_put("/proc/stb/video/zapmode", mode_zapping[zapping_mode], strlen(mode_zapping[zapping_mode]));
 		break;
 	}
 	if (p) {

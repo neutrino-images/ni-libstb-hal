2cab94ce6d75575cbe7a975d94f10566d2d663ee
vanhofen <vanhofen@gmx.de>
2021-05-09 21:25:43 +0200

    - add missing has_button_vformat for some boxmodels

--- libarmbox/hardware_caps.c
+++ libarmbox/hardware_caps.c
@@ -88,6 +88,7 @@ hw_caps_t *get_hwcaps(void)
 	caps.display_has_statusline = 0;	// 0 because we use graphlcd/lcd4linux
 	caps.display_has_colon = 0;
 	caps.has_button_timer = 1;
+	caps.has_button_vformat = 0;
 	caps.has_HDMI = 2;
 	caps.has_HDMI_input = 1;
 	strcpy(caps.startup_file, "STARTUP");
@@ -235,6 +236,7 @@ hw_caps_t *get_hwcaps(void)
 	caps.display_can_umlauts = 0;
 	caps.display_has_statusline = 0;
 	caps.has_button_timer = 1;
+	caps.has_button_vformat = 0;
 	caps.has_HDMI = 1;
 	strcpy(caps.startup_file, "STARTUP_LINUX");
 	strcpy(caps.boxvendor, "AX-Technologies");

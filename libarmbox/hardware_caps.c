0c1da94b702444480cc76762cbf77889d6c4d6f6
vanhofen <vanhofen@gmx.de>
2021-07-12 13:02:10 +0200

    - multiboxse: fix display properties

--- libarmbox/hardware_caps.c
+++ libarmbox/hardware_caps.c
@@ -280,13 +280,13 @@ hw_caps_t *get_hwcaps(void)
 	caps.can_cec = 1;
 	caps.can_cpufreq = 0;
 	caps.can_shutdown = 1;
-	caps.display_xres = 4;
+	caps.display_xres = 0;
 	caps.display_type = HW_DISPLAY_NONE;
 	caps.display_can_deepstandby = 0;
 	caps.display_can_set_brightness = 0;
 	caps.display_can_umlauts = 0;
 	caps.display_has_statusline = 0;
-	caps.display_has_colon = 1;
+	caps.display_has_colon = 0;
 	caps.has_button_timer = 1;
 	caps.has_button_vformat = 0;
 	caps.has_HDMI = 1;

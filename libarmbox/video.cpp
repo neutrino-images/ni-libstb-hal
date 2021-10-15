5be90e98da3e906a01d45434db67dc9042450c1a
BPanther <bpanther_ts@hotmail.com>
2021-10-15 14:54:57 +0200

    small change QuadPiP

--- libarmbox/video.cpp
+++ libarmbox/video.cpp
@@ -919,6 +919,18 @@ void cVideo::QuadPiP(bool active, int _x, int _y, int _w, int _h)
 			proc_put(VMPEG_dst_apply[i], buffer, strlen(buffer));
 		}
 	} else {
+		for (unsigned int i = 0; i < 4; i++) {
+			sprintf(buffer, "%x", 0);
+			proc_put(VMPEG_dst_left[i], buffer, strlen(buffer));
+			sprintf(buffer, "%x", 0);
+			proc_put(VMPEG_dst_top[i], buffer, strlen(buffer));
+			sprintf(buffer, "%x", 0);
+			proc_put(VMPEG_dst_width[i], buffer, strlen(buffer));
+			sprintf(buffer, "%x", 0);
+			proc_put(VMPEG_dst_height[i], buffer, strlen(buffer));
+			sprintf(buffer, "%x", _a);
+			proc_put(VMPEG_dst_apply[i], buffer, strlen(buffer));
+		}
 		proc_put("/proc/stb/video/decodermode", "normal", strlen("normal"));
 	}
 }

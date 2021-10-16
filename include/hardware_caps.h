d20c8c8a8bac6d4ac0881bd97ab5b810986fb7cd
vanhofen <vanhofen@gmx.de>
2021-10-16 21:00:06 +0200

    - hw-caps: resort

--- include/hardware_caps.h
+++ include/hardware_caps.h
@@ -41,9 +41,9 @@ typedef struct hw_caps
 	display_type_t display_type;
 	int display_xres;   /* x resolution or chars per line */
 	int display_yres;
+	int display_can_umlauts;
 	int display_can_deepstandby;
 	int display_can_set_brightness;
-	int display_can_umlauts;
 	int display_has_statusline;
 	int display_has_colon;
 	int has_button_timer;

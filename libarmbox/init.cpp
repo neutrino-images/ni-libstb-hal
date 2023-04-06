9132cf0fbe9d343130ae07f17bac44423f8adc27
BPanther <bpanther_ts@hotmail.com>
2023-04-06 13:03:21 +0200

    vu4k: fcc disabled

--- libarmbox/init.cpp
+++ libarmbox/init.cpp
@@ -39,7 +39,7 @@ void hal_api_init()
 		sprintf(buffer, "%x", 1);
 		proc_put("/proc/stb/fb/dst_apply", buffer, strlen(buffer));
 #if BOXMODEL_VUSOLO4K || BOXMODEL_VUDUO4K || BOXMODEL_VUDUO4KSE || BOXMODEL_VUULTIMO4K || BOXMODEL_VUUNO4KSE || BOXMODEL_VUUNO4K
-		sprintf(buffer, "%s", "enable");
+		sprintf(buffer, "%s", "disable");
 		proc_put("/proc/stb/frontend/fbc/fcc", buffer, strlen(buffer));
 		proc_put("/proc/stb/video/decodermode", "normal", strlen("normal"));
 #endif

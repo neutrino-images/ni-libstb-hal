7c9b530f2c9301dbac8e5c33164cc721c0035240
GetAway <get-away@t-online.de>
2026-02-14 18:37:00 +0100

    fix linker error while build with debug options

---
+++ libeplayer3/output/graphic_window.c
@@ -0,0 +1,14 @@
+#include "../include/common.h"
+
+static int32_t g_windows_width = 1280;
+static int32_t g_windows_height = 720;
+
+int32_t GetGraphicWindowWidth()
+{
+    return g_windows_width;
+}
+
+int32_t GetGraphicWindowHeight()
+{
+    return g_windows_height;
+}
